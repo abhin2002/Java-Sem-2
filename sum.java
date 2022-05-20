bdd56c152d5c77044c7e2668d3f7c79333b637b5
abhin2002 <95518898+abhin2002@users.noreply.github.com>
2022-05-20 15:31:33 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class HelloWorld {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two numbers\n");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.println("Sum : "+ sum);
+        
+    }
+}
