22605d30d02dbdeb8e8d1eb960393a805e4f2f9e
abhin2002 <95518898+abhin2002@users.noreply.github.com>
2022-05-20 14:44:12 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        int a=5;
+        int b=3;
+        System.out.println(a+b);
+        //System.out.println("\n");
+        System.out.println("Hello, World!"); 
+    }
+}
